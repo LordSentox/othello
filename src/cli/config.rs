365df1b71c613ffe14922c9aeb590c19e3e0fa86
Arne Dußin <arne.dussin@live.de>
2017-08-03 11:24:34 +0200

    Added client configuration file

---
+++ src/cli/config.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io;
+use toml;
+
+lazy_static! {
+	static ref CONFIG: Config = {
+		match Config::load() {
+			Ok(conf) => conf,
+			Err(err) => panic!("Could not find configuration file 'client.toml'")
+		}
+	}
+}
+
+pub enum ReadError {
+	IO(io::Error),
+	TOML(toml::de::Error)
+}
+
+#[derive(Deserialize)]
+pub struct Network {
+	server_ip: String,
+	server_port: u16,
+	login_name: String
+}
+
+#[derive(Deserialize)]
+pub struct Graphics {
+	board: String,
+	stone: String
+}
+
+// Holds the client configuration.
+#[derive(Deserialize)]
+pub struct Config {
+	network: Network,
+	graphics: Graphics
+}
+
+impl Config {
+	// Load the configuration from the toml file dedicated to the server.
+	pub fn load() -> Result<Config, ReadError> {
+		let mut file = match File::open("client.toml") {
+			Ok(file) => file,
+			Err(err) => return Err(ReadError::IO(err))
+		};
+
+		let mut contents = String::new();
+		match file.read_to_string(&mut contents) {
+			Ok(_) => {},
+			Err(err) => return Err(ReadError::IO(err))
+		}
+
+		match toml::from_str(&contents) {
+			Ok(conf) => Ok(conf),
+			Err(err) => Err(ReadError::TOML(err))
+		}
+	}
+}
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,3 +1,6 @@
+pub mod config;
+pub use self::config::*;
+
 pub mod drawing;
 pub use self::drawing::*;
 
@@ -12,3 +15,6 @@ pub use self::nethandler::NetHandler;
 
 pub mod request_game_sequence;
 pub use self::request_game_sequence::*;
+
+pub mod textures;
+pub use self::textures::*;
